f42b13f79040e25f35b7d1f1f6725832ea145531
TonyGandarilla <AntonioAGB@outlook.com>
2024-05-15 15:36:05 -0700

    added based role access level security

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,12 @@
 import React, { useEffect } from "react";
 import { Auth } from "@supabase/auth-ui-react";
 import { useNavigate } from "react-router-dom";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { ThemeSupa, darkThemes } from "@supabase/auth-ui-shared";
 import supabase from "../config/supabaseClient";
-import "../css/index.css";
-
+import "../css/index.css"; // Ensure this is correctly linked
 
 function Login() {
+  
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -31,26 +31,35 @@ function Login() {
   }, [navigate]);
 
   return (
-    <div>
-      
-         <div>
-          <h1 align="center"> Iniciar Sesion</h1>
-          </div>   
-          <div align="center">
-          <Auth
+    <div className="login-container">
+      <div className="auth-component">
+        <header className="login-header">
+          <h1 >Iniciar Sesión</h1>
+        </header> 
+        <Auth
           supabaseClient={supabase}
           providers={["google"]}
           magicLink={true}
           appearance={{ theme: ThemeSupa }}
-          theme="dark"
-          
-          
-        />
-        </div>
-        
+          theme="white"
+          localization={{
+            variables: {
+              sign_in: {
+                email_label: 'Correo electronico',
+                password_label: 'Contraseña',
+                email_input_placeholder	: 'Ingresa tu correo electronico',
+                password_input_placeholder : 'Ingresa tu contraseña',
+                social_provider_text : 'Iniciar sesion con {{provider}}',
+                button_label : 'Iniciar sesion',
+                link_text : 'No tienes una cuenta? Crea una.',
 
+              },
+            },
+          }}
+        />
+      </div>
     </div>
-  );
+  );  
 }
 
 export default Login;
---
+++ supabase/migrations/custom_access_token_hook.sql
@@ -0,0 +1,48 @@
+create or replace function public.custom_access_token_hook(event jsonb)
+returns jsonb
+language plpgsql
+as $$
+  declare
+    claims jsonb;
+    is_admin boolean;
+  begin
+    -- Check if the user is marked as admin in the profiles table
+    select is_admin into is_admin from profiles where user_id = (event->>'user_id')::uuid;
+
+    -- Proceed only if the user is an admin
+    if is_admin then
+      claims := event->'claims';
+
+      -- Check if 'app_metadata' exists in claims
+      if jsonb_typeof(claims->'app_metadata') is null then
+        -- If 'app_metadata' does not exist, create an empty object
+        claims := jsonb_set(claims, '{app_metadata}', '{}');
+      end if;
+
+      -- Set a claim of 'admin'
+      claims := jsonb_set(claims, '{app_metadata, admin}', 'true');
+
+      -- Update the 'claims' object in the original event
+      event := jsonb_set(event, '{claims}', claims);
+    end if;
+
+    -- Return the modified or original event
+    return event;
+  end;
+$$;
+
+grant execute
+  on function public.custom_access_token_hook
+  to supabase_auth_admin;
+
+revoke execute
+  on function public.custom_access_token_hook
+  from authenticated, anon;
+
+grant all
+  on table public.profiles
+  to supabase_auth_admin;
+
+revoke all
+  on table public.profiles
+  from authenticated, anon;
