9a9e609acfc9ef12eaad049014237a2e2a88784e
QQcodee <castawebdesign@gmail.com>
2024-05-15 17:00:53 -0600

    header arreglado

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,7 +6,6 @@ import supabase from "../config/supabaseClient";
 import "../css/index.css"; // Ensure this is correctly linked
 
 function Login() {
-  
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -32,79 +31,62 @@ function Login() {
 
   return (
     <div>
-        
-        <div className="login-container">
-        
-             
-              <div className="auth-component">
-              <header className="login-header">
-                <h1>Iniciar Session</h1>
-                </header>
-              <Auth
-              supabaseClient={supabase}
-              providers={["google"]}
-              magicLink={true}
-              appearance={{theme : ThemeSupa}}
-              theme="minimal"
-              localization={{
-                variables: {
-                  sign_in: {
-                    email_label: 'Correo electronico',
-                    password_label: 'Contraseña',
-                    email_input_placeholder: 'Tu correo electronico',
-                    password_input_placeholder: 'Tu contraseña',
-                    button_label: 'Iniciar Sesion',
-                    loading_button_label: 'Iniciando Sesion...',
-                    social_provider_text: 'Inicia Sesion con {{provider}}',
-                    link_text: 'Ya tenes una cuenta? Iniciar Sesion',
-                    confirmation_text: 'Checa tu email para confirmar tu cuenta',
-
-
-                  },
-                  sign_up: {
-                    email_label: 'Correo electronico',
-                    password_label: 'Contraseña',
-                    email_input_placeholder: 'Tu correo electronico',
-                    password_input_placeholder: 'Crear Contraseña',
-                    link_text: 'No tenes una cuenta? Crear una',
-                    button_label: 'Crear Cuenta',
-
-                  },
-                  forgotten_password: {
-                    link_text: 'Olvidas tu contraseña?',
-                    email_label: 'Correo electronico',
-                    email_input_placeholder: 'Tu correo electronico',
-                    button_label: 'Enviar instrucciones para recuperar tu contraseña',
-                  },
-                  magic_link: {
-                    link_text: 'Enviar link magico',
-                  },
-                  update_password: {
-                    password_label: 'Contraseña',
-                    password_input_placeholder: 'Tu nueva contraseña',
-                    button_label: 'Cambiar Contraseña',
-                    loading_button_label: 'Cambiando Contraseña...',
-                    confirmation_text: 'Tu contraseña ha sido cambiada',
-
-                  }
-
-
+      <div className="login-container">
+        <div className="auth-component">
+          <header className="login-header">
+            <h1>Iniciar Session</h1>
+          </header>
+          <Auth
+            supabaseClient={supabase}
+            providers={["google"]}
+            magicLink={true}
+            appearance={{ theme: ThemeSupa }}
+            theme="minimal"
+            localization={{
+              variables: {
+                sign_in: {
+                  email_label: "Correo electronico",
+                  password_label: "Contraseña",
+                  email_input_placeholder: "Tu correo electronico",
+                  password_input_placeholder: "Tu contraseña",
+                  button_label: "Iniciar Sesion",
+                  loading_button_label: "Iniciando Sesion...",
+                  social_provider_text: "Inicia Sesion con {{provider}}",
+                  link_text: "Ya tenes una cuenta? Iniciar Sesion",
+                  confirmation_text: "Checa tu email para confirmar tu cuenta",
                 },
-              }}
-              
-              
-            />
+                sign_up: {
+                  email_label: "Correo electronico",
+                  password_label: "Contraseña",
+                  email_input_placeholder: "Tu correo electronico",
+                  password_input_placeholder: "Crear Contraseña",
+                  link_text: "No tenes una cuenta? Crear una",
+                  button_label: "Crear Cuenta",
+                },
+                forgotten_password: {
+                  link_text: "Olvidas tu contraseña?",
+                  email_label: "Correo electronico",
+                  email_input_placeholder: "Tu correo electronico",
+                  button_label:
+                    "Enviar instrucciones para recuperar tu contraseña",
+                },
+                magic_link: {
+                  link_text: "Enviar link magico",
+                },
+                update_password: {
+                  password_label: "Contraseña",
+                  password_input_placeholder: "Tu nueva contraseña",
+                  button_label: "Cambiar Contraseña",
+                  loading_button_label: "Cambiando Contraseña...",
+                  confirmation_text: "Tu contraseña ha sido cambiada",
+                },
+              },
+            }}
+          />
         </div>
       </div>
-        
-
-              },
-            },
-          }}
-        />
-      </div>
     </div>
-  );  
+  );
 }
 
 export default Login;
